a9e874810a8c4665c4dd088e45b2c927ddaf811e
BugNatpakan <somboonnatpacan@gmail.com>
2025-04-29 14:34:03 +0700

    update server.js

--- server.js
+++ server.js
@@ -67,10 +67,9 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
     PORT, 
-    console.log(
-        "Server running in",
-        process.env.NODE_ENV,
-        "on" + `${process.env.HOST} mode on port ${PORT}`)
+    '0.0.0.0', 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+
     );
 
 const swaggerOptions ={
@@ -83,7 +82,7 @@ const swaggerOptions ={
         },
         servers:[
             {
-                url: process.env.HOST + ':' + PORT + '/api/v1'
+                url: 'http://localhost:5001'
             }
         ],
     },
@@ -97,3 +96,5 @@ process.on('unhandledRejection',(err,promise)=>{
     console.log(`Error: ${err.message}`);
     server.close(()=>process.exit(1));
 });
+
+module.exports = app;
